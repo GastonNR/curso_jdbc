e7b0d985408cf70eb03ef00cbbb66540767c10ee
nico-ruiz <nicoruiz987@gmail.com>
2025-07-07 22:11:22 -0300

    Cambios en el frame de listado de alumnos y en el controlador general

--- src/main/java/controller/ControladorGeneral.java
+++ src/main/java/controller/ControladorGeneral.java
@@ -3,12 +3,57 @@ package controller;
 import view.DetalleAlumnos;
 import view.ListaAlumnosFrame;
 
-public class ControladorGeneral {
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ControladorGeneral implements ActionListener {
     private ListaAlumnosFrame listaAlumnosFrame;
     private DetalleAlumnos detalleAlumnos;
 
     public ControladorGeneral() {
         this.listaAlumnosFrame = new ListaAlumnosFrame();
         listaAlumnosFrame.setVisible(true);
+
+        listaAlumnosFrame.getBtn_nuevo().addActionListener(this);
+        listaAlumnosFrame.getBtn_editar().addActionListener(this);
+        listaAlumnosFrame.getBtn_borrar().addActionListener(this);
+        listaAlumnosFrame.getBtn_guardar().addActionListener(this);
+        listaAlumnosFrame.getBtn_cancelar().addActionListener(this);
+
+        cargarListaAlumnos();
     }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == listaAlumnosFrame.getBtn_nuevo()) registrarAlumno();
+        if (e.getSource() == listaAlumnosFrame.getBtn_editar()) actualizarAlumno();
+        if (e.getSource() == listaAlumnosFrame.getBtn_borrar()) borrarAlumno();
+        if (e.getSource() == listaAlumnosFrame.getBtn_guardar()) guardarAlumno();
+        if (e.getSource() == listaAlumnosFrame.getBtn_cancelar()) cancelar();
+    }
+    private void registrarAlumno() {
+
+    }
+
+    private void actualizarAlumno() {
+    }
+
+    private void borrarAlumno() {
+    }
+
+    private void guardarAlumno() {
+    }
+
+    private void cancelar() {
+    }
+
+    private void cargarListaAlumnos() {
+        String[] columnas ={"ID", "Nombre", "Apellidos", "Fecha de nacimiento"};
+        DefaultTableModel modelo_tabla = new DefaultTableModel(columnas, 0);
+
+
+    }
+
 }
--- src/main/java/view/ListaAlumnosFrame.java
+++ src/main/java/view/ListaAlumnosFrame.java
@@ -1,8 +1,20 @@
 package view;
 
+import javax.swing.*;
+
 public class ListaAlumnosFrame extends javax.swing.JFrame {
 
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(ListaAlumnosFrame.class.getName());
+
+    private javax.swing.JButton btn_borrar;
+    private javax.swing.JButton btn_cancelar;
+    private javax.swing.JButton btn_editar;
+    private javax.swing.JButton btn_guardar;
+    private javax.swing.JButton btn_nuevo;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tablaAlumnos;
+    private javax.swing.JToolBar jToolBar1;
 
     public ListaAlumnosFrame() {
         initComponents();
@@ -13,15 +25,19 @@ public class ListaAlumnosFrame extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        tablaAlumnos = new javax.swing.JTable();
         jToolBar1 = new javax.swing.JToolBar();
-        jButton1 = new javax.swing.JButton();
+        btn_nuevo = new javax.swing.JButton();
+        btn_editar = new javax.swing.JButton();
+        btn_borrar = new javax.swing.JButton();
+        btn_guardar = new javax.swing.JButton();
+        btn_cancelar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        tablaAlumnos.setModel(new javax.swing.table.DefaultTableModel(
                 new Object [][] {
                         {null, null, null, null},
                         {null, null, null, null},
@@ -32,45 +48,69 @@ public class ListaAlumnosFrame extends javax.swing.JFrame {
                         "Title 1", "Title 2", "Title 3", "Title 4"
                 }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(tablaAlumnos);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
                 jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                                .addContainerGap(25, Short.MAX_VALUE)
-                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 669, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(21, 21, 21))
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(14, 14, 14)
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 517, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(12, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
                 jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addContainerGap()
-                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addContainerGap(37, Short.MAX_VALUE))
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 290, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(14, Short.MAX_VALUE))
         );
 
         jToolBar1.setBackground(new java.awt.Color(0, 51, 51));
         jToolBar1.setRollover(true);
 
-        jButton1.setText("jButton1");
-        jButton1.setFocusable(false);
-        jButton1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        jButton1.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        jToolBar1.add(jButton1);
+        btn_nuevo.setText("Nuevo");
+        btn_nuevo.setFocusable(false);
+        btn_nuevo.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        btn_nuevo.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jToolBar1.add(btn_nuevo);
+
+        btn_editar.setText("Editar");
+        btn_editar.setFocusable(false);
+        btn_editar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        btn_editar.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jToolBar1.add(btn_editar);
+
+        btn_borrar.setText("Borrar");
+        btn_borrar.setFocusable(false);
+        btn_borrar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        btn_borrar.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jToolBar1.add(btn_borrar);
+
+        btn_guardar.setText("Guardar");
+        btn_guardar.setFocusable(false);
+        btn_guardar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        btn_guardar.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jToolBar1.add(btn_guardar);
+
+        btn_cancelar.setText("Cancelar");
+        btn_cancelar.setFocusable(false);
+        btn_cancelar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        btn_cancelar.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jToolBar1.add(btn_cancelar);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jToolBar1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addComponent(jToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -79,10 +119,27 @@ public class ListaAlumnosFrame extends javax.swing.JFrame {
         pack();
     }
 
-    private javax.swing.JButton jButton1;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
-    private javax.swing.JToolBar jToolBar1;
+    public JButton getBtn_borrar() {
+        return btn_borrar;
+    }
 
+    public JButton getBtn_cancelar() {
+        return btn_cancelar;
+    }
+
+    public JButton getBtn_editar() {
+        return btn_editar;
+    }
+
+    public JButton getBtn_guardar() {
+        return btn_guardar;
+    }
+
+    public JButton getBtn_nuevo() {
+        return btn_nuevo;
+    }
+
+    public JTable gettablaAlumnos() {
+        return tablaAlumnos;
+    }
 }
